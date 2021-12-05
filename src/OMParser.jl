9649ff2aef91a21805a89d29f9c51442ad2b1131
John Tinnerholm <jtinnerholm@gmail.com>
2021-12-05 13:49:02 +0100

    Load parser libraries on other systems

--- src/OMParser.jl
+++ src/OMParser.jl
@@ -19,11 +19,21 @@ end
 
 const _libpath = if Sys.iswindows()
   local instDir = INSTALLATION_DIRECTORY_PATH
-  throw("Not fixed yet...")
-  #  joinpath(instDir, "lib", "ext", "libomparse-julia.dll")
-else
+  @static if VERSION >= v"1.5.1"
+    joinpath(instDir, "lib", "ext", "libomparse-julia_v151.dll")
+  elseif VERSION >= v"1.4.0"
+    joinpath(instDir, "lib", "ext", "libomparse-julia_v140.dll")
+  else
+    joinpath(instDir, "lib", "ext", "libomparse-julia.dll")
+  end
+elseif Sys.islinux()
   local instDir = INSTALLATION_DIRECTORY_PATH
   joinpath(instDir, "lib", "build","lib", "x86_64-linux-gnu", "libomparse-julia.so")
+elseif Sys.isapple()
+  local instDir = INSTALLATION_DIRECTORY_PATH
+  joinpath(instDir, "lib", "build","lib", "x86_64-linux-gnu", "libomparse-julia.dylib")
+else
+  throw("Your system is not supported. Supported Systems are Linux, macOS and Windows.")
 end
 
 function parseFile(fileName::String, acceptedGram::Int64 = 1)::Absyn.Program
